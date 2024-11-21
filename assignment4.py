63d67c2dfc497e6c7fe016e57a471498c66a44ee
marcobt15 <mtoito03@gmail.com>
2024-11-20 21:26:32 -0500

    initial commit

---
+++ assignment4.py
@@ -0,0 +1,23 @@
+# Name this file assignment4.py when you submit
+
+class bag_of_words_model:
+
+  def __init__(self, directory):
+    # directory is the full path to a directory containing trials through state space
+	
+    # Return nothing
+
+
+  def tf_idf(self, document_filepath):
+    # document_filepath is the full file path to a test document
+
+    # Return the term frequency-inverse document frequency vector for the document
+    return tf_idf_vector
+
+
+  def predict(self, document_filepath, weights):
+    # document_filepath is the full file path to a test document
+    # weights is a list of weights for the artificial neuron
+
+    # Return the prediction from the neural network model
+    return prediction
