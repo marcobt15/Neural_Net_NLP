5d9deb057f6c6629f1cb36d7e53199269d4e8c7b
marcobt15 <mtoito03@gmail.com>
2024-11-22 00:07:38 -0500

    init method done

--- assignment4.py
+++ assignment4.py
@@ -1,10 +1,29 @@
 # Name this file assignment4.py when you submit
+import math
+import os
 
 class bag_of_words_model:
 
   def __init__(self, directory):
     # directory is the full path to a directory containing trials through state space
-	
+    words_map = {}
+    document_count = 0
+    for file_name in os.listdir(directory):
+      document_count+=1
+      with open(os.path.join(directory, file_name), 'r') as file:
+        words = file.read().strip().split(" ")
+        for word in words:
+          words_map[word] = words_map.get(word, 0) + 1
+
+    self.vocabulary = list(words_map.keys())
+    self.vocabulary.sort()
+
+    self.idf = []
+    for word in self.vocabulary:
+      word_freq = words_map[word]
+      idf_val = math.log(document_count/word_freq, 2)
+      self.idf.append(idf_val)
+    
     # Return nothing
 
 
@@ -21,3 +40,5 @@ class bag_of_words_model:
 
     # Return the prediction from the neural network model
     return prediction
+  
+bag_of_words_model("Examples\\Example0\\training_documents")
